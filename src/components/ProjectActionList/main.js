af34b7dd1a8283fd0298cacd69233d5c4a89412c
mistkafka <mistkafka@gmail.com>
2017-03-04 10:05:43 +0800

    project list: register top action

--- src/components/ProjectActionList/main.js
+++ src/components/ProjectActionList/main.js
@@ -1,4 +1,4 @@
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters, mapMutations } from 'vuex'
 
 export default {
   name: 'ProjectActionList',
@@ -12,9 +12,32 @@ export default {
       projectActions: 'projectActions'
     })
   },
+  methods: {
+    ...mapMutations({
+      'registerTopActions': 'registerTopActions'
+    }),
+    toProjects () {
+      this.$router.push('/projects')
+    },
+    editList () {
+      console.log('TODO: editList')
+    },
+    registerAction () {
+      let actions = {
+        left: {title: 'Projects', action: this.toProjects},
+        middle: {title: '', action: null},
+        right: {title: 'Edit', action: this.editList}
+      }
+
+      this.registerTopActions(actions)
+    }
+  },
   watch: {
     '$route': function(to, from) {
       this.id = from.params.id
     }
+  },
+  beforeMount () {
+    this.registerAction()
   }
 }
